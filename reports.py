b00c7985c0f31719da80105b3797a5112a561931
matyasi.zsofia <zsofia.matyasi@gmail.com>
2021-01-02 18:36:41 +0100

    get_latest(file_name) implemented

--- reports.py
+++ reports.py
@@ -3,20 +3,24 @@ import printing
 
 
 RELEASE_YEAR = 2
+TITLE = 0
 
 
-def count_games(file_name):
+def convert_file_lines_to_list(file_name):
     file = open(file_name)
     lines = file.readlines()
     file.close()
+    return lines
+
+
+def count_games(file_name):
+    lines = convert_file_lines_to_list(file_name)
     return len(lines)
 
 
 def decide(file_name, year):
     contains = False
-    file = open(file_name)
-    lines = file.readlines()
-    file.close()
+    lines = convert_file_lines_to_list(file_name)
     for line in lines:
         properties = line.split("\t")
         if int(properties[RELEASE_YEAR]) == year:
@@ -26,7 +30,15 @@ def decide(file_name, year):
 
 
 def get_latest(file_name):
-    pass
+    latest_year = 0
+    latest_game = ''
+    lines = convert_file_lines_to_list(file_name)
+    for line in lines:
+        properties = line.split("\t")
+        if int(properties[RELEASE_YEAR]) > latest_year:
+            latest_year = int(properties[RELEASE_YEAR])
+            latest_game = properties[TITLE]
+    return latest_game
 
 
 def count_by_genre(file_name, genre):
@@ -50,4 +62,4 @@ def when_was_top_sold_fps(file_name):
 
 
 if __name__ == '__main__':
-    print(decide("game_stat.txt", 1984))
+    print(get_latest("game_stat.txt"))
